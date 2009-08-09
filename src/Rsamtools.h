93e8234976bf67312290dee6db65fe42cd699393
Martin Morgan <mtmorgan@fhcrc.org>
2009-08-09 13:29:56 +0000

    C code reorganization
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Rsamtools@41113 bc3139a8-67e5-0310-9ffc-ced21a209358

--- src/Rsamtools.h
+++ src/Rsamtools.h
@@ -10,4 +10,12 @@ SEXP scan_bam_open(SEXP fname, SEXP mode);
 SEXP scan_bam_template();
 SEXP scan_bam(SEXP bfile, SEXP template_list, SEXP space, SEXP flag);
 
+/* utilities.c */
+SEXP _get_namespace(const char *pkg);
+SEXP _get_strand_levels();
+void _as_factor_SEXP(SEXP vec, SEXP lvls);
+void _as_factor(SEXP vect, const char **lvls, const int n_lvls);
+void _reverse(char *buf);
+void _reverseComplement(char *buf);
+
 #endif /* _RSAMTOOLS_H_ */
---
+++ src/utilities.c
@@ -0,0 +1,74 @@
+#include "Rsamtools.h"
+
+SEXP
+_get_namespace(const char* pkg)
+{
+    SEXP fun = PROTECT(findFun(install("getNamespace"), R_GlobalEnv));
+    SEXP nmspc = PROTECT(NEW_CHARACTER(1));
+    SET_STRING_ELT(nmspc, 0, mkChar(pkg));
+    nmspc = eval(lang2(fun, nmspc), R_GlobalEnv);
+    UNPROTECT(2);
+    return nmspc;
+}
+
+
+SEXP
+_get_strand_levels()
+{
+    SEXP nmspc = PROTECT(_get_namespace("ShortRead"));
+    SEXP ans = eval(findVar(install(".STRAND_LEVELS"), nmspc), nmspc);
+    UNPROTECT(1);
+    return ans;
+}
+
+void
+_as_factor_SEXP(SEXP vec, SEXP lvls)
+{
+    SEXP cls = PROTECT(NEW_CHARACTER(1));
+    SET_STRING_ELT(cls, 0, mkChar("factor"));
+    SET_CLASS(vec, cls);
+    SET_ATTR(vec, install("levels"), lvls);
+    UNPROTECT(1);
+}
+
+void
+_as_factor(SEXP vec, const char **lvls, const int n_lvls)
+{
+    SEXP levels = PROTECT(NEW_CHARACTER(n_lvls));
+    int i;
+    for (i = 0; i < n_lvls; ++i)
+        SET_STRING_ELT(levels, i, mkChar(lvls[i]));
+    _as_factor_SEXP(vec, levels);
+    UNPROTECT(1);
+}
+
+void
+_reverse(char *buf)
+{
+    size_t len = strlen(buf);
+    int i;
+    char tmp;
+    for (i = 0; i < floor(len / 2); ++i) {
+        tmp = buf[len-i-1];
+        buf[len-i-1] = buf[i];
+        buf[i] = tmp;
+    }
+}
+
+void
+_reverseComplement(char *buf)
+{
+    static const int MAX_MAP = 256;
+    static char map[256];
+    static int init = 0;
+    if (init == 0) {
+        init = 1;
+        for (int i = 0; i < MAX_MAP; ++i)
+            map[i] = (char) i;
+        map['A'] = 'T'; map['C'] = 'G'; map['G'] = 'C'; map['T'] = 'A';
+        map['a'] = 't'; map['c'] = 'g'; map['g'] = 'c'; map['t'] = 'a';
+    }
+    _reverse(buf);
+    for (int i = 0; i < strlen(buf); ++i)
+        buf[i] = map[(int) buf[i]];
+}
