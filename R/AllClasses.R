473d12363f6d4dc37c257ae478c697807917e4a7
Martin Morgan <mtmorgan@fhcrc.org>
2009-09-18 16:13:41 +0000

    reorganize connections class definitions; correct doc cross-link error
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Rsamtools@41749 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -1,3 +1,13 @@
+setOldClass("connection")
+setOldClass(c("file", "connection"))
+setOldClass(c("url", "connection"))
+setOldClass(c("gzfile", "connection"))
+setOldClass(c("bzfile", "connection"))
+setOldClass(c("unz", "connection"))
+setOldClass(c("file", "connection"))
+setOldClass(c("pipe", "connection"))
+setOldClass(c("fifo", "connection"))
+
 setGeneric(".validity", function(object) standardGeneric(".validity"))
 
 setClass("ScanBamParam",
