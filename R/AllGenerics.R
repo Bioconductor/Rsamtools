b16aaedb502bbc6a26dc02870585934fb4c7592b
Martin Morgan <mtmorgan@fhcrc.org>
2009-09-18 15:54:54 +0000

    readPileup to read samtools pileup -cv files
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Rsamtools@41747 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/AllGenerics.R
+++ R/AllGenerics.R
@@ -3,3 +3,6 @@ setGeneric("scanBam",
 
 setGeneric("countBam",
            function(file, index=file, ...) standardGeneric("countBam"))
+
+setGeneric("readPileup",
+           function(file, ...) standardGeneric("readPileup"))
---
+++ R/readPileup.R
@@ -0,0 +1,88 @@
+setOldClass("connection")
+setOldClass(c("file", "connection"))
+setOldClass(c("url", "connection"))
+setOldClass(c("gzfile", "connection"))
+setOldClass(c("bzfile", "connection"))
+setOldClass(c("unz", "connection"))
+setOldClass(c("file", "connection"))
+setOldClass(c("pipe", "connection"))
+setOldClass(c("fifo", "connection"))
+
+.readPileup_table <-
+    function(conn, colClasses, ...)
+{
+    read.table(conn, colClasses=colClasses,
+               col.names=names(colClasses), sep="\t", header=FALSE,
+               quote="", comment.char="", fill=TRUE, ...)
+}
+
+.readPileup_indel_idx <- function(df) which(df[[3]] == "*")
+
+.readPileup_SNP <-
+    function(file, ..., variant)
+{
+    colClasses <-
+        c(space="factor", position="integer",
+          referenceBase="character", consensusBase="character",
+          consensusQuality="integer", snpQuality="integer",
+          maxMappingQuality="integer", coverage="integer", "NULL",
+          "NULL", "NULL", "NULL", "NULL", "NULL", "NULL")
+    dat <- .readPileup_table(file, colClasses, ...)
+    idx <- .readPileup_indel_idx(dat)
+    if (variant == "SNP")
+        idx <- c(idx, idx-1)
+    dat <- dat[-idx,]
+    RangedData(ranges=IRanges(start=dat[,2],end=dat[,2]),
+               referenceBase=factor(dat[,3], levels=DNA_ALPHABET),
+               consensusBase=factor(dat[,4], levels=DNA_ALPHABET),
+               consensusQuality=dat[,5],
+               snpQuality=dat[,6],
+               maxMappingQuality=dat[,7],
+               coverage=dat[,8],
+               space=dat[,1])
+}
+
+.readPileup_indel <-
+    function(file, ...)
+{
+    colClasses <- c(space="factor", position="integer",
+                    reference="character", consensus="character",
+                    consensusQuality="integer", snpQuality="integer",
+                    maxMappingQuality="integer", coverage="integer",
+                    alleleOne="character", alleleTwo="character",
+                    alleleOneSupport="integer", alleleTwoSupport="integer",
+                    additionalIndels="integer", "NULL", "NULL")
+    dat <- .readPileup_table(file, colClasses, ...)
+    idx <- .readPileup_indel_idx(dat)
+
+    idx <- which(dat[[3]] == "*")
+    dat0 <- dat[idx-1,]
+    dat <- dat[idx,]
+
+    RangedData(ranges=IRanges(start=dat0[,2],end=dat0[,2]),
+               referenceBase=factor(dat0[,3], levels=DNA_ALPHABET),
+               consensusBase=factor(dat0[,4]),
+               consensusQuality=dat0[,5],
+               snpQuality=dat0[,6],
+               maxMappingQuality=dat0[,7],
+               coverage=dat0[,8],
+               alleleOne=dat[,9],
+               alleleOneSupport=dat[,11],
+               alleleTwo=dat[,10],
+               alleleTwoSupport=dat[,12],
+               additionalIndels=dat[,13],
+               space=dat[,1])
+}
+
+setMethod(readPileup, "connection",
+          function(file, ..., variant=c("SNP", "indel", "all"))
+{
+    variant <- match.arg(variant)
+    switch(variant, SNP=, all=.readPileup_SNP(file=file, ...,
+           variant=variant), indel=.readPileup_indel(file=file, ...))
+})
+
+setMethod(readPileup, "character", function(file, ...)
+{
+    callGeneric(file(file, "r"), ...)
+})
