97a9dfc4d0939f465af9b2e80a1084eaf11465fb
Martin Morgan <mtmorgan@fhcrc.org>
2009-09-29 19:51:05 +0000

    update remote_test.R script, and Makevars to make _FILE_OFFSET_BITS=64 not universal
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Rsamtools@42019 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/scripts/remote_test.R
+++ inst/scripts/remote_test.R
@@ -1,4 +1,4 @@
-library(Rsamtools)
+suppressMessages(library(Rsamtools))
 fl = "ftp://ftp-trace.ncbi.nih.gov/1000genomes/ftp/pilot_data/data/NA19240/alignment/NA19240.chrom6.SLX.maq.SRP000032.2009_07.bam"
 p1 <- ScanBamParam(which=RangesList("6"=IRanges(10000, 11000)))
 res <- scanBam(fl, param=p1)[[1]]
@@ -9,7 +9,7 @@ p1 <- ScanBamParam(which=RangesList("6"=IRanges(100000, 110000)))
 res <- scanBam(fl, param=p1)[[1]]
 res[["seq"]]
 
-header <- readBamHeader(fl)
+header <- scanBamHeader(fl)
 txt <- header[[1]][[2]]
 table(names(txt))
 txt[names(txt) == "@HD"]                # 'header'
