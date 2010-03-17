f6e77b643020eaeb9fd7de99e8c9918ef72bdfc8
Martin Morgan <mtmorgan@fhcrc.org>
2010-03-17 16:24:14 +0000

    'rname' was converted to integer if any range is a factor of length 1
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Rsamtools@45287 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/readBAMasGappedAlignments.R
+++ R/readBAMasGappedAlignments.R
@@ -12,14 +12,13 @@ setMethod(readBAMasGappedAlignments, "character",
     bam <- scanBam(file, index=index, param=param)
     ## unlist(list(factor())) returns integer(0), so exit early if all
     ## values are empty
-    if (all(sapply(bam, function(x) length(x$rname) == 0)))
-        return(Alignments0())
+    bam <- bam[sapply(bam, function(x) length(x$rname) != 0)]
+    if (0L == length(bam))
+        return(get(ans.subtype)())
     rname <- unlist(unname(lapply(bam, "[[", "rname")))
     strand <- unlist(unname(lapply(bam, "[[", "strand")))
     pos <- unlist(unname(lapply(bam, "[[", "pos")))
-    cigar <-
-        unlist(unname(lapply(bam, function(x) as.character(cigars(x$cigar)))))
+    cigar <- unlist(unname(lapply(bam, "[[", "cigar")))
     ## Calls the appropriate constructor.
     get(ans.subtype)(rname=rname, strand=strand, pos=pos, cigar=cigar)
 })
-
