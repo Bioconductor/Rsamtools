fec38fbee35ea0560ef957a32825f50a36cd51bc
Martin Morgan <mtmorgan@fhcrc.org>
2010-04-05 17:21:47 +0000

    less dire warnings about state of Rsamtools development
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Rsamtools@45744 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -5,14 +5,4 @@
 {
     if (!identical(levels(strand()), .STRAND_LEVELS))
         stop("internal: 'levels(strand())' not consistent with Rsamtools")
-
-    msg <- "Rsamtools is experimental; expect frequent changes
-            in data types and functionality.
-
-            Issues with Windows remote-file retrieval exist; this
-            is not yet reliable."
-
-    message("\n",
-            paste(strwrap(msg, indent=2, exdent=2), collapse="\n"),
-            "\n")
 }
