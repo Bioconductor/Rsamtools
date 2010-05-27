efbe49b65fe75c9aa351b47b678ea9393b428e4f
Martin Morgan <mtmorgan@fhcrc.org>
2010-05-27 21:22:11 +0000

    Check that local files exist before reading
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Rsamtools@47178 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/io_bam.R
+++ R/io_bam.R
@@ -1,6 +1,11 @@
 .io_bam <-
     function(func, file, index, ..., param)
 {
+    idx <- !grepl("^(ftp)|(http)://", file)
+    if (!all(sapply(file[idx], file.exists))) {
+        msg <- paste(sprintf("'%s'", file[idx]), collapse="\n  ")
+        stop("'file' elements do not exist:\n  ", msg)
+    }
     file <- .normalizePath(file)
     index <- .normalizePath(index)
     flag <- bamFlag(param)
