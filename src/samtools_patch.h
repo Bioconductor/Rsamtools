9eaa0dfcc3560fd98cca755acc52035a4653545e
Martin Morgan <mtmorgan@fhcrc.org>
2011-03-22 17:40:29 +0000

    move error handling patches into bam, bcf libraries
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Rsamtools@53943 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ src/samtools_patch.h
@@ -0,0 +1,9 @@
+#ifndef SAMTOOLS_PATCH_H
+#define SAMTOOLS_PATCH_H
+
+/* capture samtools errors */
+extern void _samtools_abort();
+extern void _samtools_exit(int status);
+extern int _samtools_fprintf (FILE *, const char *, ...);
+
+#endif
--- src/utilities.c
+++ src/utilities.c
@@ -1,43 +1,6 @@
 #include <Rdefines.h>
 #include "utilities.h"
 
-void
-_samtools_exit(int status)
-{
-    Rf_error("internal: samtools invoked 'exit(%d)'; see warnings() and restart R", 
-	     status);
-}
-
-void
-_samtools_abort()
-{
-    Rf_error("internal: samtools invoked 'abort'; see warnings() and restart R");
-}
-
-int
-_samtools_fprintf(FILE *file, const char *fmt, ...)
-{
-    static const int bufsize = 2048;
-    va_list argp;
-    int n;
-
-    if (stderr != file) {
-	va_start(argp, fmt);
-	n = vfprintf(file, fmt, argp);
-	va_end(argp);
-    } else {
-	/* silence some messages */
-	char *buf = (char *) R_alloc(bufsize, sizeof(char));
-	if (0 == strncmp("[samopen] SAM header is present:", fmt, 32))
-	    return 0;
-	va_start(argp, fmt);
-	n = vsnprintf(buf, bufsize, fmt, argp);
-	va_end(argp);
-	Rf_warning(buf);
-    }
-    return n;
-}
-
 SEXP
 _get_namespace(const char* pkg)
 {
