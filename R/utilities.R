9a8e1522d6918c9dc223f5e0209d6ad9e4cf08d4
Martin Morgan <mtmorgan@fhcrc.org>
2011-04-19 14:16:25 +0000

    make .ppath (print paths spanning options(width)) a stand-alone function
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Rsamtools@54931 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/utilities.R
+++ R/utilities.R
@@ -1,3 +1,15 @@
+.ppath <- function(tag, filepath)
+{
+    wd <- options('width')[[1]] - nchar(tag) - 6
+    if (0L == length(filepath) || nchar(filepath) < wd)
+        return(sprintf("%s: %s\n", tag, filepath))
+    bname <- basename(filepath)
+    wd1 <- wd - nchar(bname)
+    dname <- substr(dirname(filepath), 1, wd1)
+    sprintf("%s: %s...%s%s\n",
+            tag, dname, .Platform$file.sep, bname)
+}
+
 .io_check_exists <-
     function(file)
 {
