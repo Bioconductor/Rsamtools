09a1dedcea4fa19884b21b2b6d9ef24408569e4d
Martin Morgan <mtmorgan@fhcrc.org>
2011-04-21 17:27:12 +0000

    second attempt at correcting Rsamtools.mk
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Rsamtools@55020 bc3139a8-67e5-0310-9ffc-ced21a209358

--- src/Rsamtools.mk
+++ src/Rsamtools.mk
@@ -1,7 +1,8 @@
+# over-written except for Windows
 SAMTOOLS_PATH=\
     $(shell echo 'cat(system.file("usrlib", .Platform[["r_arch"]],\
                          package="Rsamtools", mustWork=TRUE))' |\
                  "${R_HOME}/bin/R" --vanilla --slave)
-PKG_LIBS+=-L"$(SAMTOOLS_PATH)" -lbam -lbcf -ltabix -lRzlib
+PKG_LIBS+=-L"$(SAMTOOLS_PATH)" -lbam -lbcf $(ZLIB_LIBS) -lws2_32
 PKG_CPPFLAGS+=-D_USE_KNETFILE -D_FILE_OFFSET_BITS=64 \
 	-D_LARGEFILE64_SOURCE
