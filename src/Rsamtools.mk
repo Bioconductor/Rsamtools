0b574b2bf2a70f392945315f998ee902f941cb80
Herve Pages <hpages@fhcrc.org>
2011-05-27 06:12:14 +0000

    rolling back unintentional previous change to Rsamtools.mk
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Rsamtools@55858 bc3139a8-67e5-0310-9ffc-ced21a209358

--- src/Rsamtools.mk
+++ src/Rsamtools.mk
@@ -2,6 +2,6 @@ SAMTOOLS_PATH=\
     $(shell echo 'cat(system.file("usrlib", .Platform[["r_arch"]],\
                          package="Rsamtools", mustWork=TRUE))' |\
                  "${R_HOME}/bin/R" --vanilla --slave)
-PKG_LIBS+=-L"$(SAMTOOLS_PATH)" -lbam -lbcf -ltabix -lz 
+PKG_LIBS+=-L"$(SAMTOOLS_PATH)" -lbam -lbcf $(ZLIB_LIBS) -lws2_32
 PKG_CPPFLAGS+=-D_USE_KNETFILE -D_FILE_OFFSET_BITS=64 \
 	-D_LARGEFILE64_SOURCE
