002fbf754b27ba1a3a6f167cc4d0601e5dbd3908
Martin Morgan <mtmorgan@fhcrc.org>
2011-06-10 02:41:00 +0000

    use ` instead of $(shell, as per R-exts
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Rsamtools@56091 bc3139a8-67e5-0310-9ffc-ced21a209358

--- src/Rsamtools.mk
+++ src/Rsamtools.mk
@@ -1,17 +1,17 @@
 # over-written except for Windows
 SAMTOOLS_PATH=\
-    $(shell echo 'cat(system.file("usrlib", .Platform[["r_arch"]],\
+    `echo 'cat(system.file("usrlib", .Platform[["r_arch"]],\
                          package="Rsamtools", mustWork=TRUE))' |\
-                 "${R_HOME}/bin/R" --vanilla --slave)
+                 "${R_HOME}/bin/R" --vanilla --slave`
 PKG_LIBS+=-L"$(SAMTOOLS_PATH)" -lbam -lbcf -ltabix -lws2_32
 PKG_CPPFLAGS+=-D_USE_KNETFILE -D_FILE_OFFSET_BITS=64 \
 	-D_LARGEFILE64_SOURCE
 
 # zlib
-ZLIB_CFLAGS+=$(shell echo 'zlibbioc::pkgconfig("PKG_CFLAGS")'|\
-    "${R_HOME}/bin/R" --vanilla --slave)
-PKG_LIBS+=$(shell echo 'zlibbioc::pkgconfig("PKG_LIBS_shared")' |\
-    "${R_HOME}/bin/R" --vanilla --slave)
+ZLIB_CFLAGS+=`echo 'zlibbioc::pkgconfig("PKG_CFLAGS")'|\
+    "${R_HOME}/bin/R" --vanilla --slave`
+PKG_LIBS+=`echo 'zlibbioc::pkgconfig("PKG_LIBS_shared")' |\
+    "${R_HOME}/bin/R" --vanilla --slave`
 %.o: %.c
 	$(CC) $(ZLIB_CFLAGS) $(ALL_CPPFLAGS) $(ALL_CFLAGS) -c $< -o $@
 
