c80512342e0cf395bbc373c1a2635f617f2042ca
Martin Morgan <mtmorgan@fhcrc.org>
2011-07-24 22:18:00 +0000

    import file / connection classes from rtracklayer
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Rsamtools@56945 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -1,11 +1,4 @@
-setOldClass("connection")
-setOldClass(c("file", "connection"))
-setOldClass(c("url", "connection"))
-setOldClass(c("gzfile", "connection"))
 setOldClass(c("bzfile", "connection"))
-setOldClass(c("unz", "connection"))
-setOldClass(c("file", "connection"))
-setOldClass(c("pipe", "connection"))
 setOldClass(c("fifo", "connection"))
 
 setGeneric(".validity", function(object) standardGeneric(".validity"))
