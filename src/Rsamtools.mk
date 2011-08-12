63eec01b2a94798b08636270c1ec7351280e7ba9
Marc Carlson <mcarlson@fhcrc.org>
2011-08-12 22:56:23 +0000

    Moves tabulateSummarizedExperiment from GenomicRanges.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Rsamtools@57420 bc3139a8-67e5-0310-9ffc-ced21a209358

--- src/Rsamtools.mk
+++ src/Rsamtools.mk
@@ -1,17 +1,7 @@
-# over-written except for Windows
 SAMTOOLS_PATH=\
     `echo 'cat(system.file("usrlib", .Platform[["r_arch"]],\
                          package="Rsamtools", mustWork=TRUE))' |\
                  "${R_HOME}/bin/R" --vanilla --slave`
-PKG_LIBS+=-L"$(SAMTOOLS_PATH)" -lbam -lbcf -ltabix -lws2_32
+PKG_LIBS+=-L"$(SAMTOOLS_PATH)" -lbam -lbcf -ltabix -lz 
 PKG_CPPFLAGS+=-D_USE_KNETFILE -D_FILE_OFFSET_BITS=64 \
 	-D_LARGEFILE64_SOURCE
-
-# zlib
-ZLIB_CFLAGS+=`echo 'zlibbioc::pkgconfig("PKG_CFLAGS")'|\
-    "${R_HOME}/bin/R" --vanilla --slave`
-PKG_LIBS+=`echo 'zlibbioc::pkgconfig("PKG_LIBS_shared")' |\
-    "${R_HOME}/bin/R" --vanilla --slave`
-%.o: %.c
-	$(CC) $(ZLIB_CFLAGS) $(ALL_CPPFLAGS) $(ALL_CFLAGS) -c $< -o $@
-
