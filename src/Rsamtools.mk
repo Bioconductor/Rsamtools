606a4e66c8a68ff8dba5d117feb713703dd56343
Martin Morgan <mtmorgan@fhcrc.org>
2011-09-26 17:06:04 +0000

    update Makevars and included source
    - absolute path to shared object locations for linking
    - Rsamtools.mk.in for non-Windows, Rsamtools.mk.win for Windows
    - Rsamtools.mk.win links to zlibbioc, overrides .o: .cc, .o: .cpp
    - compiler patterns
    - remove zlib source -- using zlibbioc instead
    
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Rsamtools@58382 bc3139a8-67e5-0310-9ffc-ced21a209358

--- src/Rsamtools.mk
+++
@@ -1,7 +0,0 @@
-SAMTOOLS_PATH=\
-    `echo 'cat(system.file("usrlib", .Platform[["r_arch"]],\
-                         package="Rsamtools", mustWork=TRUE))' |\
-                 "${R_HOME}/bin/R" --vanilla --slave`
-PKG_LIBS+=-L"$(SAMTOOLS_PATH)" -lbam -lbcf -ltabix -lz 
-PKG_CPPFLAGS+=-D_USE_KNETFILE -D_FILE_OFFSET_BITS=64 \
-	-D_LARGEFILE64_SOURCE
