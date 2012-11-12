a007f37e716162b04a9f7cf26d30f3d2e028bf37
Michael Lawrence <lawremi@iastate.edu>
2012-11-12 23:41:52 +0000

    Added a 'param' (ScanBamParam) argument to coverage,BamFile
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Rsamtools@71144 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/methods-BamFile.R
+++ R/methods-BamFile.R
@@ -407,8 +407,9 @@ setMethod("findSpliceOverlaps", c("BamFile", "ANY"),
 ###
 
 setMethod("coverage", "BamFile",
-          function(x, shift=0L, width=NULL, weight=1L, ...)
-          coverage(readBamGappedAlignments(x),
+          function(x, shift=0L, width=NULL, weight=1L, param = ScanBamParam(),
+                   ...)
+          coverage(readBamGappedAlignments(x, param = param),
                    shift=shift, width=width, weight=weight, ...)
           )
 
