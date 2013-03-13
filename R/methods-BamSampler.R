bb066a95692e0f9f7c58c047526793a4b44781ad
Martin Morgan <mtmorgan@fhcrc.org>
2013-03-13 20:22:29 +0000

    add BamSampler class
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Rsamtools@74307 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/methods-BamSampler.R
@@ -0,0 +1,41 @@
+.BamSampler <- setRefClass("BamSampler", contains="BamFile")
+
+BamSampler <-
+    function (file, index = file, ..., yieldSize, obeyQname = FALSE) 
+{
+    .RsamtoolsFile(.BamSampler, .normalizePath(file), .normalizePath(index), 
+        yieldSize = yieldSize, obeyQname = obeyQname, ...)
+}
+
+setMethod("scanBam", "BamSampler",
+    function(file, index=file, ...,
+             param=ScanBamParam(what=scanBamWhat()))
+{
+    tot <- sampleSize <- yieldSize(file)
+    if (is.na(yieldSize(file)))
+        stop("'yieldSize' must not be NA")
+
+    bfile <- as(file, "BamFile")
+    open(bfile, "rb")
+    on.exit(close(bfile))
+
+    smpl <- .quickUnlist(scanBam(bfile, param=param))
+    repeat {
+        yld <- .quickUnlist(scanBam(bfile, param=param))
+        yld_n <- length(yld[[1]])
+        if (length(yld[[1]]) == 0L)
+            break
+        tot <- tot + yld_n
+        keep <- rbinom(1L, yld_n, yld_n/ tot)
+        if (keep == 0L)
+            next
+
+        i <- sample(sampleSize, keep)
+        j <- sample(yld_n, keep)
+        smpl <- Map(function(x, y, i, j) {
+            x[i] <- y[j]
+            x
+        }, smpl, yld, MoreArgs=list(i=i, j=j))
+    }
+    list(smpl)
+})
---
+++ inst/unitTests/test_BamSampler.R
@@ -0,0 +1,34 @@
+fl <- system.file("extdata", "ex1.bam", package="Rsamtools")
+
+test_BamSampler <- function()
+{
+    bf <- BamSampler(fl, yieldSize=1000)
+    set.seed(123L); obs1 <- scanBam(bf)
+    checkIdentical(yieldSize(bf), length(obs1[[1]][[1]]))
+    checkIdentical(741747L, sum(obs1[[1]][["pos"]], na.rm=TRUE))
+
+    set.seed(123L); obs2 <- scanBam(bf)
+    checkIdentical(obs1, obs2)
+
+    obs2 <- scanBam(bf)
+    checkTrue(!identical(obs1, obs2))
+
+    bf <- BamSampler(fl, yieldSize=10000)
+    checkIdentical(scanBam(fl), scanBam(bf))
+}
+
+test_BamSampler_param <- function()
+{
+
+    bf <- BamSampler(fl, yieldSize=1000)
+    param <- ScanBamParam(what="strand", flag=scanBamFlag(
+                                           isUnmappedQuery=FALSE,
+                                           isMinusStrand=FALSE))
+    obs <- scanBam(bf, param=param)[[1]][["strand"]]
+    checkTrue(all(obs == "+"))
+    checkIdentical(yieldSize(bf), length(obs))
+
+    bf <- BamSampler(fl, yieldSize=10000)
+    obs <- scanBam(bf, param=param)[[1]][["strand"]]
+    checkIdentical(1647L, length(obs))
+}
