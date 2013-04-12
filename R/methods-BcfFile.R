4c2d3a3cb5f1a92e286719b4d78c5c80e73d5f35
Martin Morgan <mtmorgan@fhcrc.org>
2013-04-12 20:05:37 +0000

    no need to grep for an exact match -- integer values
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Rsamtools@75614 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/methods-BcfFile.R
+++ R/methods-BcfFile.R
@@ -87,10 +87,10 @@ setMethod(isOpen, "BcfFile",
         cols <- unique(unlist(keys))
         entries <- Map(function(k) as.vector(sapply(vals, "[", k)),
                        cols)
-        desc <- "DESCRIPTION" == toupper(names(entries))
+        desc <- which("DESCRIPTION" == toupper(names(entries)))
         if (1L == length(desc))
             entries[[desc]] <- gsub("\"", "", entries[[desc]])
-        id <- "ID" == toupper(names(entries))
+        id <- which("ID" == toupper(names(entries)))
         if (length(id)) {
             df <- do.call(DataFrame, entries[-id])
             if (any(duplicated(entries[[id]]))) 
