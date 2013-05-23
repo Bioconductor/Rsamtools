f73b940651788f58eaaf91e78f40bebd1e5373c4
Valerie Obenchain <vobencha@fhcrc.org>
2013-05-23 21:05:00 +0000

    Fix how 'missing' column is computed in 'colData' of SummarizedExperiment. Cannot use filterBam() on a qname-sorted file.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Rsamtools@76842 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/methods-BamFile.R
+++ R/methods-BamFile.R
@@ -540,18 +540,12 @@ setMethod("summarizeOverlaps", c("GRangesList", "BamFile"),
                inter.feature, param
            ) 
 
-
-
     counts <- as.matrix(do.call(cbind, cts))
     countBam <- countBam(reads)
-    colData <- DataFrame(countBam[c("records", "nucleotides")],
-                         row.names=countBam$file) 
     param <- ScanBamParam(flag=scanBamFlag(isUnmappedQuery=FALSE), what="seq")
-    mapped <- lapply(reads, function(fl, param) {
-                             dest <- filterBam(fl, tempfile(), param=param)
-                             countBam(dest)$records
-                         }, param)
-    colData$mapped <- do.call(c, mapped)
+    colData <- DataFrame(countBam[c("records", "nucleotides")],
+                         mapped=countBam(reads, param=param)$records,
+                         row.names=countBam$file) 
     SummarizedExperiment(assays=SimpleList(counts=counts),
                          rowData=features, colData=colData)
 }
