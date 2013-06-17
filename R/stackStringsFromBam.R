24c702a963e361d46e2a6fd8d8a9156619765744
Herve Pages <hpages@fhcrc.org>
2013-06-17 06:41:12 +0000

    sequenceLayer() now allows to lay sequences belonging to 1 of the 3 supported spaces (query/reference/pairwise) to any of the other 3 spaces.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Rsamtools@77616 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/stackStringsFromBam.R
+++ R/stackStringsFromBam.R
@@ -5,6 +5,8 @@
 
 .make_empty_sequences <- function(skeleton, class="BStringSet")
 {
+    if (is.null(skeleton))
+        return(NULL)
     skeleton <- PartitioningByEnd(skeleton)
     skeleton_len <- length(skeleton)
     if (skeleton_len == 0L) {
@@ -42,6 +44,10 @@
 ### Parallel combine.
 .pcombine <- function(x, y)
 {
+    if (is.null(x))
+        return(y)
+    if (is.null(y))
+        return(x)
     if (length(x) != length(y))
         stop("'x' and 'y' must have the same length")
     xy <- c(x, y)
@@ -56,59 +62,87 @@
 ### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 ### sequenceLayer()
 ###
-### NOTE: Using the same letter ("-") for gaps (N) and deletions (D) is
-###       problematic when one tries to infer coverage by calling
-###       consensusMatrix() on the result of sequenceLayer(): then gaps
-###       and deletions will both generate coverage when only the latters
-###       should! Hence the result won't be consistent with coverage().
-###       OTOH using "+" for gaps is compatible with coverage() but is
-###       not visually appealing (the gaps, which are usually big, cannot
-###       be seen anymore).
-###       "-" and "+" are the only 2 special letters in Biostrings DNA
-###       alphabet (DNA_ALPHABET). Should we add 1 more? Should it be " ",
-###       ".", or "*"?
 
-sequenceLayer <- function(x, cigar, layout="query-to-reference",
+.D_and_N_ranges_on_query_space <- function(cigar)
+{
+    ans1 <- cigarRangesOnQuerySpace(cigar, ops="D")
+    ans2 <- cigarRangesOnQuerySpace(cigar, ops="N")
+    .pcombine(ans1, ans2)
+}
+
+.D_and_N_fillers <- function(cigar, D.letter, N.letter)
+{
+    D_width <- width(cigarRangesOnReferenceSpace(cigar, ops="D"))
+    N_width <- width(cigarRangesOnReferenceSpace(cigar, ops="N"))
+    ans1 <- .make_sequence_fillers_from_list_of_widths(D_width, D.letter)
+    ans2 <- .make_sequence_fillers_from_list_of_widths(N_width, N.letter)
+    .pcombine(ans1, ans2)
+}
+
+.I_or_S_fillers <- function(cigar, ops, letter)
+{
+    ops_width <- width(cigarRangesOnQuerySpace(cigar, ops=ops))
+    .make_sequence_fillers_from_list_of_widths(ops_width, letter)
+}
+
+sequenceLayer <- function(x, cigar, from="query", to="reference",
                           D.letter="-", N.letter="-")
 {
     if (!is(x, "XStringSet"))
         stop("'x' must be an XStringSet object")
-    LAYOUTS <- c("query-to-reference",
-                 "reference-to-query",
-                 "aligned-query",
-                 "aligned-reference")
-    layout <- match.arg(layout, LAYOUTS)
+    LAYOUTS <- c("reference", "query", "pairwise")
+    from <- match.arg(from, LAYOUTS)
+    to <- match.arg(to, LAYOUTS)
     D.letter <- Biostrings:::.normarg_padding.letter(D.letter, seqtype(x))
     N.letter <- Biostrings:::.normarg_padding.letter(N.letter, seqtype(x))
-    ops1 <- c("I", "S")
-    ops2 <- c("D", "N")
-    if (layout == "query-to-reference") {
-        where_to_remove <- cigarRangesOnQuery(cigar, ops=ops1)
-        where_to_fill1 <- cigarRangesOnQuery(cigar, ops="D")
-        where_to_fill2 <- cigarRangesOnQuery(cigar, ops="N")
-        where_to_fill <- .pcombine(where_to_fill1, where_to_fill2)
-        pos_one <- rep.int(1L, length(cigar))
-        filler_widths1 <- width(cigarRangesOnReference(cigar, pos=pos_one,
-                                                       ops="D"))
-        filler_widths2 <- width(cigarRangesOnReference(cigar, pos=pos_one,
-                                                       ops="N"))
-        fillers1 <- .make_sequence_fillers_from_list_of_widths(
-                                        filler_widths1, D.letter)
-        fillers2 <- .make_sequence_fillers_from_list_of_widths(
-                                        filler_widths2, N.letter)
-        fillers <- .pcombine(fillers1, fillers2)
-    } else if (layout == "reference-to-query") {
-        filler_widths <- width(cigarRangesOnQuery(cigar, ops=ops1))
-        fillers <- .make_sequence_fillers_from_list_of_widths(
-                                        filler_widths, as("-", class(x))[[1L]])
-        pos_one <- rep.int(1L, length(cigar))
-        where_to_fill <- cigarRangesOnReference(cigar, pos=pos_one, ops=ops1)
-        where_to_remove <- cigarRangesOnReference(cigar, pos=pos_one, ops=ops2)
+    if (from == to)
+        return(x)
+    if (from == "query") {
+        if (to == "reference") {
+            ## From "query" to "reference".
+            ops_to_remove <- c("I", "S")
+        } else {
+            ## From "query" to "pairwise".
+            ops_to_remove <- "S"
+        }
+        remove_at <- cigarRangesOnQuerySpace(cigar, ops=ops_to_remove)
+        inject_at <- .D_and_N_ranges_on_query_space(cigar)
+        fillers <- .D_and_N_fillers(cigar, D.letter, N.letter)
+    } else if (from == "reference") {
+        if (to == "query") {
+            ## From "reference" to "query".
+            ops_to_remove <- c("D", "N")
+            remove_at <- cigarRangesOnReferenceSpace(cigar, ops=ops_to_remove)
+            ops_to_inject <- c("I", "S")
+            inject_at <- cigarRangesOnReferenceSpace(cigar, ops=ops_to_inject)
+            letter <- as("-", class(x))[[1L]]
+            fillers <- .I_or_S_fillers(cigar, ops_to_inject, letter)
+        } else {
+            ## From "reference" to "pairwise".
+            remove_at <- NULL
+            ops_to_inject <- "I"
+            inject_at <- cigarRangesOnReferenceSpace(cigar, ops=ops_to_inject)
+            letter <- as("-", class(x))[[1L]]
+            fillers <- .I_or_S_fillers(cigar, ops_to_inject, letter)
+        }
     } else {
-        stop("\"", layout, "\" layout is not implemented yet, sorry!")
+        if (to == "query") {
+            ## From "pairwise" to "query".
+            ops_to_remove <- c("D", "N")
+            remove_at <- cigarRangesOnPairwiseSpace(cigar, ops=ops_to_remove)
+            ops_to_inject <- "S"
+            inject_at <- cigarRangesOnPairwiseSpace(cigar, ops=ops_to_inject)
+            letter <- as("-", class(x))[[1L]]
+            fillers <- .I_or_S_fillers(cigar, ops_to_inject, letter)
+        } else {
+            ## From "pairwise" to "reference".
+            ops_to_remove <- "I"
+            remove_at <- cigarRangesOnPairwiseSpace(cigar, ops=ops_to_remove)
+            inject_at <- fillers <- NULL
+        }
     }
-    at <- .pcombine(where_to_remove, where_to_fill)
-    empty_sequences <- .make_empty_sequences(where_to_remove, class=class(x))
+    at <- .pcombine(remove_at, inject_at)
+    empty_sequences <- .make_empty_sequences(remove_at, class=class(x))
     value <- .pcombine(empty_sequences, fillers)
     replaceAt(x, at, value=value)
 }
@@ -162,6 +196,17 @@ sequenceLayer <- function(x, cigar, layout="query-to-reference",
     param
 }
 
+### NOTE: Using the same letter ("-") for gaps (N) and deletions (D) is
+###       problematic when one tries to infer coverage by calling
+###       consensusMatrix() on the result of sequenceLayer(): then gaps
+###       and deletions will both generate coverage when only the latters
+###       should! Hence the result won't be consistent with coverage().
+###       OTOH using "+" for gaps is compatible with coverage() but is
+###       not visually appealing (the gaps, which are usually big, cannot
+###       be seen anymore).
+###       "-" and "+" are the only 2 special letters in Biostrings DNA
+###       alphabet (DNA_ALPHABET). Should we add 1 more? Should it be " ",
+###       ".", or "*"?
 stackStringsFromBam <- function(file, param, use.names=FALSE, what="seq",
                                 D.letter="-", N.letter="-",
                                 Lpadding.letter="+", Rpadding.letter="+")
@@ -201,25 +246,37 @@ if (FALSE) {
 
 library(Rsamtools)
 
-x <- DNAStringSet(c(seq1="AAAA", seq2="AAAAATCCCTTTTNN"))
-cigar <- c("4M", "5M1I3M2D4M2S")
+qseq <- DNAStringSet(c(seq1="nnAAAACCTTGAAA", seq2="AAAAATCCCTTTTnn"))
+cigar <- c("2S4M1D2M2I1M5N3M", "5M1I3M2D4M2S")
 
-### Check compatibility between 'x' and 'cigar' (because this is not cheap,
+### Check compatibility between 'qseq' and 'cigar' (because this is not cheap,
 ### sequenceLayer() doesn't do it):
-stopifnot(identical(width(x), cigarToQWidth(cigar)))
+stopifnot(identical(width(qseq), cigarToQWidth(cigar)))
 
-target <- DNAStringSet(c(seq1="AAAA", seq2="AAAAACCC--TTTT"))
-### A quick sanity check before we start testing sequenceLayer():
-stopifnot(identical(width(target), cigarToWidth(cigar)))
+rseq0 <- DNAStringSet(c(seq1="AAAA-CCG-----AAA", seq2="AAAAACCC--TTTT"))
+pseq0 <- DNAStringSet(c(seq1="AAAA-CCTTG-----AAA", seq2="AAAAATCCC--TTTT"))
+### 1 quick sanity check before we start testing sequenceLayer():
+stopifnot(identical(width(rseq0), cigarToWidth(cigar)))
 
-### Testing sequenceLayer():
-x2 <- sequenceLayer(x, cigar, layout="query-to-reference")
-stopifnot(all(target == x2))
-x3 <- sequenceLayer(x2, cigar, layout="reference-to-query")
-stopifnot(all(width(x) == width(x3)))
-x4 <- sequenceLayer(x3, cigar, layout="query-to-reference")
-stopifnot(all(x2 == x4))
-
+###              ~ ~ ~ ~ ~ Testing sequenceLayer() ~ ~ ~ ~ ~
+### query <-> reference
+rseq <- sequenceLayer(qseq, cigar, from="query", to="reference")
+stopifnot(all(rseq0 == rseq))
+qseq2 <- sequenceLayer(rseq, cigar, from="reference", to="query")
+stopifnot(all(width(qseq) == width(qseq2)))
+rseq2 <- sequenceLayer(qseq2, cigar, from="query", to="reference")
+stopifnot(all(rseq == rseq2))
+### query <-> pairwise
+pseq <- sequenceLayer(qseq, cigar, from="query", to="pairwise")
+stopifnot(all(pseq0 == pseq))
+qseq3 <- sequenceLayer(pseq, cigar, from="pairwise", to="query")
+stopifnot(all(width(qseq) == width(qseq3)))
+pseq2 <- sequenceLayer(qseq3, cigar, from="query", to="pairwise")
+stopifnot(all(pseq == pseq2))
+### lay query and reference to pairwise space and compare
+pseq3a <- sequenceLayer(qseq2, cigar, from="query", to="pairwise")
+pseq3b <- sequenceLayer(rseq, cigar, from="reference", to="pairwise")
+stopifnot(all(pseq3a == pseq3b))
 
 
 bamfile <- BamFile(system.file("extdata", "ex1.bam", package="Rsamtools"))
