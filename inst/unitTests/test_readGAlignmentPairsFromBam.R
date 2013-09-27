442eaf0091d2f95031ede2bf29af13b3b647b92d
Herve Pages <hpages@fhcrc.org>
2013-09-27 20:13:48 +0000

    small tweak to toy BAM file used in tests
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Rsamtools@80856 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/unitTests/test_readGAlignmentPairsFromBam.R
+++ inst/unitTests/test_readGAlignmentPairsFromBam.R
@@ -30,8 +30,8 @@ make_samline <- function(QNAME, mapped=0, RNAME="*", POS=0, strand="+",
 }
 
 make_mapped_pair <- function(QNAME,
-                             RNAME1, POS1, strand1,
-                             RNAME2, POS2, strand2,
+                             RNAME1, POS1, CIGAR1, strand1,
+                             RNAME2, POS2, CIGAR2, strand2,
                              primary, proper,
                              pair_id=NA)
 {
@@ -40,9 +40,9 @@ make_mapped_pair <- function(QNAME,
                             0x20 * (strand2 == "-")
     FLAG2 <- flag0 + 0x80 + 0x10 * (strand2 == "-") +
                             0x20 * (strand1 == "-")
-    line1 <- paste(QNAME, FLAG1, RNAME1, POS1, "255", "18M",
+    line1 <- paste(QNAME, FLAG1, RNAME1, POS1, "255", CIGAR1,
                    RNAME2, POS2, "0", "*", "*", sep="\t")
-    line2 <- paste(QNAME, FLAG2, RNAME2, POS2, "255", "18M",
+    line2 <- paste(QNAME, FLAG2, RNAME2, POS2, "255", CIGAR2,
                    RNAME1, POS1, "0", "*", "*", sep="\t")
     if (!identical(pair_id, NA)) {
         pair_id_tag <- paste0("pi:Z:", pair_id)
@@ -54,9 +54,12 @@ make_mapped_pair <- function(QNAME,
 
 make_mapped_pairs <- function(mapped_pair_table)
 {
-    if (!is.data.frame(mapped_pair_table))
+    if (!is.data.frame(mapped_pair_table)) {
+        if (is.character(mapped_pair_table))
+            mapped_pair_table <- textConnection(mapped_pair_table)
         mapped_pair_table <- read.table(mapped_pair_table, header=TRUE,
                                         stringsAsFactors=FALSE)
+    }
     row_groups <- unname(split(seq_len(nrow(mapped_pair_table)),
                                mapped_pair_table$QNAME))
 
@@ -103,7 +106,7 @@ make_toy_bamfile <- function(mapped_pair_table, filename)
         ## p991: 1 pair with a missing mate (can happen if file was subsetted
         ## with e.g. filterBam)
         make_mapped_pair("p991",
-                         "chr2", 150, "+", "chr2", 199, "-",
+                         "chr2", 150, "18M", "+", "chr2", 199, "18M", "-",
                          primary=1, proper=1)[2L],
         ## p992: 1 pair with 1st mate unmapped and 2nd mate mapped
         make_samline("p992", flagbits=0x1 + 0x40,
@@ -149,43 +152,44 @@ make_toy_bamfile <- function(mapped_pair_table, filename)
 }
 
 ### 1 line per mapped pair. Each line will generate 2 lines/records in the
-### SAM file.
-mapped_pair_table <- textConnection("
-QNAME   RNAME1  POS1    strand1 RNAME2  POS2    strand2 primary proper  pair_id
+### SAM file. The pair_id field will be stored in the SAM/BAM file as a user
+### defined tag ("pi" tag).
+mapped_pair_table <- "
+QNAME RNAME1 POS1 CIGAR1 strand1 RNAME2 POS2 CIGAR2 strand2 primary proper pair_id
 # p001: 1 primary proper pair
-p001    chr2    10      +       chr2    110     -       1       1       p001
+p001  chr2   10   18M    +       chr2   110  18M    -       1       1      p001
 # p002: 1 primary non proper pair
-p002    chr2    20      +       chr2    120     -       1       0       p002
+p002  chr2   20   18M    +       chr2   120  18M    -       1       0      p002
 # p003: 2 proper pairs: 1 primary + 1 secondary
-p003    chr2    30      +       chr2    130     -       1       1       p003a
-p003    chr2    31      +       chr2    131     -       0       1       p003b
+p003  chr2   30   18M    +       chr2   130  18M    -       1       1      p003a
+p003  chr2   31   18M    +       chr2   131  18M    -       0       1      p003b
 # p004: 2 non proper pairs: 1 primary + 1 secondary
-p004    chr2    40      +       chr2    140     -       1       0       p004a
-p004    chr2    41      +       chr2    141     -       0       0       p004b
+p004  chr2   40   18M    +       chr2   140  18M    -       1       0      p004a
+p004  chr2   41   18M    +       chr2   141  18M    -       0       0      p004b
 # p005: 2 primary pairs (some aligners seem to produce that, even though they
 # probably shouldn't)
-p005    chr2    50      +       chr2    150     -       1       1       p005a
-p005    chr2    51      +       chr2    151     -       1       1       p005b
+p005  chr2   50   18M    +       chr2   150  18M    -       1       1      p005a
+p005  chr2   51   18M    +       chr2   151  18M    -       1       1      p005b
 # p006: 3 pairs: 1 primary proper + 1 secondary proper + 1 secondary non
 # proper
-p006    chr2    60      +       chr2    160     -       1       1       p006a
-p006    chr2    61      +       chr2    161     -       0       1       p006b
-p006    chr2    62      +       chr2    60      -       0       0       p006c
+p006  chr2   60   18M    +       chr2   160  18M    -       1       1      p006a
+p006  chr2   61   18M    +       chr2   161  18M    -       0       1      p006b
+p006  chr2   62   18M    +       chr2   60   18M    -       0       0      p006c
 # p007: 2 pairs mapped to the same position: 1 primary proper + 1 secondary
 # proper
-p007    chr2    70      +       chr2    170     -       1       1       p007a
-p007    chr2    70      +       chr2    170     -       0       1       p007b
+p007  chr2   70   9M1D9M +       chr2   170  18M    -       1       1      p007a
+p007  chr2   70   18M    +       chr2   170  7M2I9M -       0       1      p007b
 # p008: 3 pairs mapped to the same position: 1 primary proper + 1 secondary
 # proper + 1 secondary non proper
-p008    chr2    80      +       chr2    180     -       1       1       p008a
-p008    chr2    80      +       chr2    180     -       0       1       p008b
-p008    chr2    80      +       chr2    180     -       0       0       p008c
+p008  chr2   80   18M    +       chr2   180  18M    -       1       1      p008a
+p008  chr2   80   9M2D9M +       chr2   180  7M2I9M -       0       1      p008b
+p008  chr2   80   6M3I9M +       chr2   180  9M3D9M -       0       0      p008c
 # p009: 3 pairs mapped to the same position: 1 primary proper + 2 secondary
 # proper. The secondary pairs can NOT be disambiguated.
-p009    chr2    90      +       chr2    190     -       1       1       p009a
-p009    chr2    90      +       chr2    190     -       0       1       p009b
-p009    chr2    90      +       chr2    190     -       0       1       p009c
-")
+p009  chr2   90   18M    +       chr2   190  18M    -       1       1      p009a
+p009  chr2   90   9M2D9M +       chr2   190  7M2I9M -       0       1      p009b
+p009  chr2   90   6M3I9M +       chr2   190  9M3D9M -       0       1      p009c
+"
 
 toy_bamfile <- make_toy_bamfile(mapped_pair_table, tempfile())
 
