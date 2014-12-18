94bd8f2716bc5e80498e642bd2d076625fa4aaa0
Martin Morgan <mtmorgan@fhcrc.org>
2014-12-18 23:19:59 +0000

    tweak testPairedEndBam
    - respect user-specified yieldSize
    - report progress
    
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Rsamtools@97785 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/testPairedEndBam.R
+++ R/testPairedEndBam.R
@@ -9,14 +9,21 @@ setMethod("testPairedEndBam", "BamFile",
     function(file, index=file, ...)
 {
     yieldSize <- yieldSize(file)
-    yieldSize(file) <- 1000000L
+    if (is.na(yieldSize))
+        yieldSize(file) <- 1000000L
     on.exit(yieldSize(file) <- yieldSize)
+    if (isOpen(file))
+        close(file)
+    open(file)
     isPaired <- FALSE
+    tot <- 0
     repeat {
         flag <- scanBam(file, param=ScanBamParam(what="flag"))[[1]]$flag
         isPaired <- any(bamFlagTest(flag, "isPaired"))
-        if (isPaired || length(flag) == 0)
+        if (isPaired || length(flag) == 0L)
             break
+        tot <- tot + length(flag)
+        message(tot, " ", appendLF=FALSE)
     }
     isPaired
 })
