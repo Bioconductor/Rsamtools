261f8de4e27882e6479be94eb0a5b2a89f0a9188
Nathaniel Hayden <nhayden@fhcrc.org>
2015-02-25 23:42:17 +0000

    unify how to add to PKG_LIBS and PKG_CPPFLAGS vars
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Rsamtools@99822 bc3139a8-67e5-0310-9ffc-ced21a209358

--- Rsamtools.mk
+++ Rsamtools.mk
@@ -2,7 +2,7 @@ SAMTOOLS_PATH=\
     `echo 'cat(system.file("usrlib", .Platform[["r_arch"]],\
                          package="Rsamtools", mustWork=TRUE))' |\
                  "${R_HOME}/bin/R" --vanilla --slave`
-PKG_LIBS+="$(SAMTOOLS_PATH)/libbam.a" "$(SAMTOOLS_PATH)/libbcf.a"\
+SAMTOOLS_LIBS="$(SAMTOOLS_PATH)/libbam.a" "$(SAMTOOLS_PATH)/libbcf.a"\
 	"$(SAMTOOLS_PATH)/libtabix.a" -lz -pthread
-PKG_CPPFLAGS+=-D_USE_KNETFILE -DBGZF_CACHE -D_FILE_OFFSET_BITS=64 \
+SAMTOOLS_CPPFLAGS=-D_USE_KNETFILE -DBGZF_CACHE -D_FILE_OFFSET_BITS=64 \
 	-D_LARGEFILE64_SOURCE
