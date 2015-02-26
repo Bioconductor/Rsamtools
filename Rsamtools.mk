af0a2c17ad5a33584a80f4a0e3e92be3b16da818
Nathaniel Hayden <nhayden@fhcrc.org>
2015-02-26 19:20:03 +0000

    Remove Rsamtools.mk (targeting Linux) and update to use SAMTOOLS_(LIBS|CPPFLAGS) in Rsamtools.mk.win
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Rsamtools@99864 bc3139a8-67e5-0310-9ffc-ced21a209358

--- Rsamtools.mk
+++
@@ -1,8 +0,0 @@
-SAMTOOLS_PATH=\
-    `echo 'cat(system.file("usrlib", .Platform[["r_arch"]],\
-                         package="Rsamtools", mustWork=TRUE))' |\
-                 "${R_HOME}/bin/R" --vanilla --slave`
-SAMTOOLS_LIBS="$(SAMTOOLS_PATH)/libbam.a" "$(SAMTOOLS_PATH)/libbcf.a"\
-	"$(SAMTOOLS_PATH)/libtabix.a" -lz -pthread
-SAMTOOLS_CPPFLAGS=-D_USE_KNETFILE -DBGZF_CACHE -D_FILE_OFFSET_BITS=64 \
-	-D_LARGEFILE64_SOURCE
