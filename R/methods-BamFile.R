fc1cde0ba9aea6bab14f1db92f14b8ff1114bb75
Martin Morgan <mtmorgan@fhcrc.org>
2015-05-31 15:06:04 +0000

    better fix for r104211 (duplicate bam header seqlevels)
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Rsamtools@104401 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/methods-BamFile.R
+++ R/methods-BamFile.R
@@ -114,7 +114,7 @@ setMethod(seqinfo, "BamFile",
           function(x)
 {
     h <- scanBamHeader(x, what="targets")[["targets"]]
-    h <- h[!duplicated(h)]
+    h <- h[!(duplicated(h) & duplicated(names(h)))]
     Seqinfo(names(h), unname(h))
 })
 
