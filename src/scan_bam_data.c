Bioconductor/Rsamtools

commit 306684d477900ddf918ff0ea6b37e1fc86d0d3fd

bugfix: don't grow NULL tags
- https://support.bioconductor.org/p/110609/

src/scan_bam_data.c
@@ -37,8 +37,9 @@ static void _grow_SCAN_BAM_DATA_tags(SEXP tags, int len)
 
     for (i = 0; i < LENGTH(tags); ++i) {
         SEXP elt = VECTOR_ELT(tags, i);
+        if (R_NilValue == elt)
+            continue;
         int len0 = LENGTH(elt);
-        elt = Rf_lengthgets(elt, len);
         SET_VECTOR_ELT(tags, i, elt);
         switch (TYPEOF(elt)) {
         case INTSXP:

commit 714bcc5dc641c210715a9a55d8d90cedd02edfcd

fix bug introduced by 306684d

src/scan_bam_data.c
@@ -40,6 +40,7 @@ static void _grow_SCAN_BAM_DATA_tags(SEXP tags, int len)
         if (R_NilValue == elt)
             continue;
         int len0 = LENGTH(elt);
+        elt = Rf_lengthgets(elt, len);
         SET_VECTOR_ELT(tags, i, elt);
         switch (TYPEOF(elt)) {
         case INTSXP: